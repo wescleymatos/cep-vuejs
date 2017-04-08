28dbbc1d417bbe6c8f9011e907208acb8a340c5e
Wescley Matos <wescleymatos@gmail.com>
2017-04-07 21:42:48 -0300

    bind de cep

---
+++ app.js
@@ -0,0 +1,11 @@
+new Vue({
+    el: '#app',
+    data: {
+        cep: '59080-100'
+    },
+    methods: {
+        getAddress: function () {
+            console.log('teste');
+        }
+    }
+});
